1962be7ff077676b35465c227b287e26613db632
krasai <krasuskip@gmail.com>
2015-09-16 14:33:36 +0200

    Changes on scripts

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,25 @@ function validateForm(e){
     var imie = document.getElementById('Imie');
     var email = document.getElementById('Email');
 
-    if(!imie.value.trim()){
-        imie.value = 'pole jest wymagane!';
+    function validateEmail(em) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(em);
+    }
+
+    if(!imie.value.trim() || imie.value == "Name..." || imie.value == "pole jest wymagane!"){
+        imie.value = 'Pole jest wymagane!';
     } else {
 
     }
 
-    if(!email.value.trim()){
-        email.value = 'pole jest wymagane!';
+    if(!email.value.trim() || email.value == "E-mail..." || email.value == "pole jest wymagane!"){
+        email.value = 'Pole jest wymagane!';
+    } else if(!validateEmail(email.value)){
+        email.value = 'Nieprawidłowy adres e-mail...';
     } else {
-
+        email.value = "Gratulujemy udało się";
+        console.log(email.value);
     }
-
-    console.log(imie + ' ' + email);
 }
 document.getElementById('submit-btn').addEventListener('click', validateForm);
+
